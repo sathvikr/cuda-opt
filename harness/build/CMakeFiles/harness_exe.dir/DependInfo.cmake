3d5760d73cc5b0c41c5051052190f208b3c0875f
Leo Nagel <leonhard3a@hotmail.de>
2025-03-01 17:26:26 +0000

    harness working

---
+++ harness/build/CMakeFiles/harness_exe.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/cuda-opt/harness/harness.cu" "CMakeFiles/harness_exe.dir/harness.cu.o" "gcc" "CMakeFiles/harness_exe.dir/harness.cu.o.d"
+  "/home/ubuntu/cuda-opt/harness/kernels/simple_kernel.cu" "CMakeFiles/harness_exe.dir/kernels/simple_kernel.cu.o" "gcc" "CMakeFiles/harness_exe.dir/kernels/simple_kernel.cu.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ harness/build/CMakeFiles/harness_exe.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/harness_exe.dir/cmake_device_link.o"
+  "CMakeFiles/harness_exe.dir/harness.cu.o"
+  "CMakeFiles/harness_exe.dir/harness.cu.o.d"
+  "CMakeFiles/harness_exe.dir/kernels/simple_kernel.cu.o"
+  "CMakeFiles/harness_exe.dir/kernels/simple_kernel.cu.o.d"
+  "harness_exe"
+  "harness_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/harness_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
